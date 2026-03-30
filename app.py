faa942e5ae868c4a5e771191656550492ce35774
Jahnea Bush <jbush0811@floridapoly.edu>
2026-03-30 16:46:27 +0000

    task c

---
+++ app.py
@@ -0,0 +1,12 @@
+if not df.empty:
+    st.divider()#separator
+    avg_grade = calculate_stats(df)
+    col1, col2 = st.columns(2)
+    with col1:
+        sts.metric(label="Average class Grade", value= f"{avg_grade:.2f}%")
+    with col2:
+        sts.metric(label="Total Students", value= len(df))
+        #display the interactive table
+        st.subheader("Student Grade List")
+        st.dataframe(df, use_container_width = True)
+
